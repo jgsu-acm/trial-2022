aaba48d2b2ee2340af4e03836fa5c38cfbf8341e
StableAgOH <stagoh17@gmail.com>
2022-10-16 16:50:20 +0800

    Add F solution

--- std/F.cpp
+++ std/F.cpp
@@ -1,11 +1,29 @@
-/* 中文注释 */
-// 中文注释
 #include <iostream>
+#include <map>
 using namespace std;
+map<int,int> mp;
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    cout<<a+b<<endl;
+    int n;
+    cin>>n;
+    int mn1=1e9, mn2=1e9;
+    for(int i=1;i<=n;i++)
+    {
+        int x;
+        cin>>x;
+        if(mp[x])
+        {
+            int t = i-mp[x];    // q-p
+            int r = mp[x]+i;    // p+q
+            if(t<mn1||(t==mn1&&r<mn2))
+            {
+                mn1 = t;
+                mn2 = r;
+            }
+            mp[x] = i;          // 更新上一次出现的位置
+        }
+        else mp[x] = i;
+    }
+    cout<<(mn2==1e9?-1:mn2)<<endl;
     return 0;
 }
